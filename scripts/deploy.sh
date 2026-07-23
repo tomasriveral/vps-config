fb2f7a78e7d4a78574a5519d1a8c5734e8b25e77
Tomás Rivera <tomas.riveral@icloud.com>
2026-07-23 20:59:34 +0200

    fix kdrive and volume

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@ set -e
 
 cd "$(dirname "$0")/.."
 
+
+systemctl status kdrive-rclone | (echo "Exiting kDrive is not running..." & exit 1)
 docker compose pull
 docker compose up -d
 docker compose run archivebox server
