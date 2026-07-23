ac5be6e55d15cef447ea086c66c0be41098ec2ed
Tomás Rivera <tomas.riveral@icloud.com>
2026-07-23 23:03:31 +0200

    fix deploy  script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$0")/.."
-
-
-systemctl status kdrive-rclone | (echo "Exiting kDrive is not running..." & exit 1)
 docker compose pull
 docker compose up -d
 docker compose run archivebox server
