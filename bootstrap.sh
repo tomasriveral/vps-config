3ce3f9498d2fb6805d92a97f2f5f477b336a7c35
Tomás Rivera <tomas.riveral@icloud.com>
2026-07-24 16:32:24 +0200

    setup uptime-kuma

--- bootstrap.sh
+++ bootstrap.sh
@@ -153,7 +153,8 @@ mkdir -p \
     /mnt/kdrive \
     /opt/server/archivebox/archivebox-data \
     /opt/server/freshrss \
-    /opt/server/filebrowser/database
+    /opt/server/filebrowser/database \
+    /opt/server/uptime-kuma
 
 sudo chown -R 1000:1000 /opt/server/filebrowser # somehow filebrowser cant acces it
 
